fb506fd6be19926002ca75a1c60d40a321e8a2ea
Vlad Tovbin <tovbinv@post.bgu.ac.il>
2022-12-11 09:58:16 +0200

    html and css updates

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting_page"),
-    path("posts", views.posts, name="posts_page"),
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail,
-         name="post_detail_page")  # /posts/my-first-post
+         name="post-detail-page")  # /posts/my-first-post
 ]
