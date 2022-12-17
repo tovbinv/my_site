934d69077f64c0fef50b67e320884f9f734f46c0
tovbinv <tovbinvlad96@gmail.com>
2022-12-17 11:42:17 +0200

    user uploads, comments, stored posts.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Tag, Author, Post
+from . models import Tag, Author, Post, Comment
 # Register your models here.
 
 
@@ -9,6 +9,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 admin.site.register(Tag)
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
